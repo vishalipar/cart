cdb15744347548ee50454d9a953d451d475efef6
vishalipar <vishalipar04@gmail.com>
2024-12-08 10:37:57 +0530

    Media configuration in settings file

--- vicart/settings.py
+++ vicart/settings.py
@@ -136,6 +136,11 @@ EMAIL_USE_TLS=True
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR /'staticfiles'
 
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
