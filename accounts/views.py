ebfc7e86059ac3fc3333338f9cf851a26a20ae42
vishalipar <vishalipar04@gmail.com>
2024-12-22 15:26:25 +0530

    Update checkout page and implement carts for authenticated and not authenticated users

--- accounts/views.py
+++ accounts/views.py
@@ -9,10 +9,12 @@ from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-# from cart.models import Cart
+from cart.views import _cart_id
+from cart.models import Cart,CartItem
 
 from .forms import RegistrationForm
 from .models import Account
+import requests
 
 # Create your views here.
 def register(request):
@@ -82,94 +84,88 @@ def activate(request, uidb64, token):
             
     
 def login(request):   
-    if request.method == 'POST': 
-        email = request.POST['email']
-        password = request.POST['password']
-        user = auth.authenticate(email=email, password=password)
-        
-        print(user, email, password)
-        
-        if user is not None:
-            auth.login(request, user)
-            return redirect('home')
-        else:
-            messages.error(request, 'Invalid login credentials.')
-            return redirect('login')
-            
-    # if request.method == 'POST':
+    # if request.method == 'POST': 
     #     email = request.POST['email']
     #     password = request.POST['password']
     #     user = auth.authenticate(email=email, password=password)
         
-    #     # for business accounts
-    #     # print(user.type)
-        
-    #     # if user.type == 'business':
-    #     #     return render(request, '')
-            
-    #     # *
+    #     print(user, email, password)
         
     #     if user is not None:
-    #         if user.type == 'user':
-    #             try:
-    #                 cart = Cart.objects.get(cart_id=_cart_id(request))
-    #                 is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
-    #                 if is_cart_item_exists:
-    #                     cart_item = CartItem.objects.filter(cart=cart)
-                        
-    #                     # getting product_variation by cart id
-    #                     product_variation = []
-    #                     for item in cart_item:
-    #                         variation = item.variations.all()
-    #                         product_variation.append(list(variation))
-                            
-    #                         # get the cart items from the user to access his product_variation
-    #                         cart_item = CartItem.objects.filter(user=user)
-    #                         ex_var_list = []
-    #                         id = []
-    #                         for item in cart_item:
-    #                             existing_variation = item.variations.all()
-    #                             ex_var_list.append(list(existing_variation))
-    #                             id.append(item.id)
-                            
-    #                         # product_variation = [1,2,3,4,6]
-    #                         # ex_var_list = [4,6,3,5]
-                                
-    #                         for pr in product_variation:
-    #                             if pr in ex_var_list:
-    #                                 index = ex_var_list.index(pr)
-    #                                 item_id = id[index]
-    #                                 item = CartItem.objects.get(id=item_id)
-    #                                 item.quantity += 1
-    #                                 item.user = user
-    #                                 item.save()
-    #                             else:
-    #                                 cart_item = CartItem.objects.filter(cart=cart)
-    #                                 for item in cart_item:
-    #                                     item.user = user
-    #                                     item.save()
-    #             except:
-    #                 pass
-    #             auth.login(request, user)
-    #             messages.success(request, "You are now logged in.")
-    #             url = request.META.get('HTTP_REFERER')
-    #             try:
-    #                 query = requests.utils.urlparse(url).query
-    #                 params = dict(x.split('=') for x in query.split('&'))
-    #                 if 'next' in params:
-    #                     nextPage = params['next']
-    #                     return redirect(nextPage)
-    #             except:
-    #                 return redirect('home')
-            
-    #         else:
-    #             messages.error(request, "You registered as business account")
-    #             return redirect('login')
-            
-            
+    #         auth.login(request, user)
+    #         return redirect('home')
     #     else:
-    #         messages.error(request, "Invalid login credentials")
-    #         return redirect("login")
+    #         messages.error(request, 'Invalid login credentials.')
+    #         return redirect('login')
+            
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = auth.authenticate(email=email, password=password)
+        
+        if user is not None:
+            if user.type == 'user':
+                try:
+                    cart = Cart.objects.get(cart_id=_cart_id(request))
+                    is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+                    if is_cart_item_exists:
+                        cart_item = CartItem.objects.filter(cart=cart)
+                        
+                        # getting product_variation by cart id
+                        product_variation = []
+                        for item in cart_item:
+                            # item.user = user
+                            # item.save()
+                            variation = item.variations.all()
+                            product_variation.append(list(variation))
+                            
+                            # # get the cart items from the user to access his product_variation
+                        cart_item = CartItem.objects.filter(user=user)
+                        ex_var_list = []
+                        id = []
+                        for item in cart_item:
+                            existing_variation = item.variations.all()
+                            ex_var_list.append(list(existing_variation))
+                            id.append(item.id)
+                            
+                            # # product_variation = [1,2,3,4,6]
+                            # # ex_var_list = [4,6,3,5]
+                                
+                        for pr in product_variation:
+                            if pr in ex_var_list:
+                                index = ex_var_list.index(pr)
+                                item_id = id[index]
+                                item = CartItem.objects.get(id=item_id)
+                                item.quantity += 1
+                                item.user = user
+                                item.save()
+                            else:
+                                cart_item = CartItem.objects.filter(cart=cart)
+                                for item in cart_item:
+                                    item.user = user
+                                    item.save()
+                except:
+                    pass
+                auth.login(request, user)
+                # messages.success(request, "You are now logged in.")
+                url = request.META.get('HTTP_REFERER')
+                try:
+                    query = requests.utils.urlparse(url).query
+                    params = dict(x.split('=') for x in query.split('&'))
+                    if 'next' in params:
+                        nextPage = params['next']
+                        return redirect(nextPage)
+                except:
+                    return redirect('home')
+            
+            else:
+                messages.error(request, "You registered as business account")
+                return redirect('login')
+            
+            
+        else:
+            messages.error(request, "Invalid login credentials")
+            return redirect("login")
     
     return render(request, 'accounts/login.html')
     
