ef8e6493e4aadcb97cb417de2e622295b86e91e7
vishalipar <vishalipar04@gmail.com>
2024-12-23 03:20:17 +0530

    Create Orders app

--- vicart/urls.py
+++ vicart/urls.py
@@ -28,5 +28,6 @@ urlpatterns = [
     path('store/', include('store.urls')),
     path('product/', include('product.urls')),
     path('cart/', include('cart.urls')),
+    path('orders/', include('orders.urls')),
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
