74c0d6185439090c0c9bfd007964cee90e435d78
vishalipar <vishalipar04@gmail.com>
2024-12-25 16:21:49 +0530

    Implement order detail page

--- accounts/views.py
+++ accounts/views.py
@@ -14,7 +14,7 @@ from cart.models import Cart,CartItem
 from .forms import RegistrationForm, UserForm, UserProfileForm
 from .models import Account,UserProfile
 import requests
-from orders.models import Order
+from orders.models import Order,OrderProduct
 
 # Create your views here.
 def register(request):
@@ -295,3 +295,18 @@ def change_password(request):
             return redirect('change_password')
             
     return render(request, 'accounts/change_password.html')
+
+@login_required(login_url='login')
+def order_detail(request, order_id):
+    order_detail = OrderProduct.objects.filter(order__order_number=order_id)
+    order = Order.objects.get(order_number = order_id)
+    subtotal = 0
+    for i in order_detail:
+        subtotal += i.product_price * i.quantity
+    context = {
+        'order_detail': order_detail,
+        'order': order,
+        'subtotal': subtotal,
+    }
+    return render(request, 'accounts/order_detail.html', context)
+
