024142aabf046674d90ec0b9e10097b36b24a141
vishalipar <vishalipar04@gmail.com>
2024-12-25 20:23:48 +0530

    Add ratings to the home page products

--- app/views.py
+++ app/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from .urls import *
-from product.models import Product
+from product.models import Product,ReviewRating
 
 def home(request):
     products = Product.objects.all()
     
+    # get the reviews
+    for product in products:
+        reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
+    
     context = {
         'products':products,
+        'reviews':reviews,
     }
     return render(request, 'home.html', context)
